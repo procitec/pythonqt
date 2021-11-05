cfd5cd1dff9caa7e7b27f5e10f37571710812a75
Alexander Kosik <akosik@t-online.de>
2021-11-05 16:30:08 +0100

    Commit complete autotest file
    This tests python, objects and contexts, WrapperFactory and class
    decorators.

--- gui_tests/test.py
+++ gui_tests/test.py
@@ -32,20 +32,68 @@ class TestPythonQt(unittest.TestCase):
             os.remove(self.pyqt_cls_dec)
 
     def test_pythoncore(self):
-        # write file
+        """
+        Test that we can call python methods. 
+        Creates a new file and checks if it was written to hard disk
+        """
         with open(self.core_fn, 'wt') as f:
             f.write("test case 1")
 
         self.assert_file_exists(self.core_fn)
 
-
     def test_pythonqt_obj(self):
-        pass
+        """
+        Test that we have a gui and a context object
+        Test the file creation with the gui object
+        """
+        self.assertTrue(gui != None)
+        self.assertTrue(meta != None)
+
+        # create file with gui object
+        gui.create_file(self.pyqt_obj)
+        self.assert_file_exists(self.pyqt_obj)
 
     def test_pythonqt_cls(self):
-        pass
+        """
+        Test the class wrapper by creating with WrapperFactory. 
+        First create a class wrapper with the create_filehandle method of meta
+        object.
+
+        Than use methods of the class wrapper to test by creating and deleting files. 
+        """
+        # create file with gui object
+        f = meta.create_filehandle(self.pyqt_cls)
+
+        # make sure file is not existent
+        f.delete_file()
+
+        # file should not exist
+        with self.assertRaises(AssertionError):
+            self.assert_file_exists(self.pyqt_cls)
+
+        # create file
+        f.create_file()
+
+        self.assertTrue(f.file_exists())
+
+        # file should exist
+        self.assert_file_exists(self.pyqt_cls)
 
     def test_pythonqt_cls_dec(self):
-        pass
+        """
+        Test the class decorator to create c++ instance WITHOUT factory. 
+        Instance only has exists() method
+        """
+        from PythonQt.example import FileDec
 
-# unittest.main(verbosity=3)
+        fd = FileDec("not_existing.txt")
+        self.assertFalse(fd.exists())
+
+        # now create file
+        with open(self.pyqt_cls_dec, 'wt') as f:
+            f.write("test case 4")
+
+        fd = FileDec(self.pyqt_cls_dec)
+        self.assertTrue(fd.exists())
+
+unittest.main(module=__name__, verbosity=3, exit=False)
